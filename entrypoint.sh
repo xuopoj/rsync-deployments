044ff433b9746b30737d019e7388d542cf46e180
Sean Xu <xuopoj@gmail.com>
2020-05-14 00:28:18 +0800

    change host

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,4 +12,4 @@ chmod 600 "$SSH_PATH/deploy_key"
 # Do deployment
 sh -c "rsync ${INPUT_RSYNC_OPTIONS} -e 'ssh -i $SSH_PATH/deploy_key -o StrictHostKeyChecking=no' $GITHUB_WORKSPACE/${INPUT_SRC:-""} ${INPUT_USER_AND_HOST}:${INPUT_DEST}"
 
-sh -c "ssh -i $SSH_PATH/deploy_key xuopoj@ali.vps 'cd workspace/doumi;npm install;sudo systemctl restart doumi'"
+sh -c "ssh -i $SSH_PATH/deploy_key ${INPUT_USER_AND_HOST} 'cd workspace/doumi;npm install;sudo systemctl restart doumi'"
